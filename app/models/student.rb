1d9c1413db4eecd6580cb4a8150f7e55ece338eb
Amy Westcott <amy.westcott@yahoo.com>
2019-12-10 21:06:05 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
 class Student < ActiveRecord::Base
+  
+  #attr_accessor :active
+  
   def to_s
-    self.first_name + " " + self.last_name
+    # binding.pry
+    if self.active 
+      self.first_name + " " + self.last_name + " This student is currently active."
+    else
+      self.first_name + " " + self.last_name + " This student is currently inactive."
+    end 
   end
+
 end
